f7b0f3ade0cc33c16cd4e4511550667ee823460c
sachi <sachidanandupadhyay7@hotmail.com>
2020-01-02 08:26:39 -0500

    Inti Project

---
+++ src/screens/AccountScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text , StyleSheet} from 'react-native';
+
+const AccountScreen =()=>{
+  return(
+     <Text>AccountScreen</Text>
+  );
+};
+
+const styles=StyleSheet.create({
+
+});
+
+export default AccountScreen;
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text , StyleSheet} from 'react-native';
+
+const SignupScreen =()=>{
+  return(
+     <Text>SignupScreen</Text>
+  );
+};
+
+const styles=StyleSheet.create({
+
+});
+
+export default SignupScreen;
